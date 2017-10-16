c6e967cfc6d4acaecf38af4e1b05e8f2b5251131
Chris <chris@usablehq.com>
2017-10-16 20:02:30 +0100

    Physics not working.

--- src/pong.js
+++ src/pong.js
@@ -1,3 +1,6 @@
+import CANNON from 'cannon';
+
+import { PerspectiveCamera } from '../node_modules/three/src/cameras/PerspectiveCamera';
 import { OrthographicCamera } from '../node_modules/three/src/cameras/OrthographicCamera';
 import { Scene } from '../node_modules/three/src/scenes/Scene';
 import { Mesh } from '../node_modules/three/src/objects/Mesh';
@@ -12,28 +15,84 @@ import { TorusBufferGeometry } from '../node_modules/three/src/geometries/TorusG
 import { MeshPhongMaterial } from '../node_modules/three/src/materials/MeshPhongMaterial';
 import { Object3D } from '../node_modules/three/src/core/Object3D';
 
-function init() {
+let world, scene, renderer, camera, player, field, fieldMesh, ball, ballMesh;
+var timeStep = 1/60;
 
-    let scene = new Scene();
+function initCannon() {
+
+    world = new CANNON.World();
+    world.gravity.set(0,0,-9.8);
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.solver.iterations = 10;
+
+
+    // Materials
+    var fieldMaterial = new CANNON.Material("fieldMaterial");
+    var ballMaterial = new CANNON.Material("ballMaterial");
+    // Adjust constraint equation parameters for ground/ground contact
+    var field_ball_cm = new CANNON.ContactMaterial(fieldMaterial, ballMaterial, {
+        friction: 0.4,
+        restitution: 0.3,
+        contactEquationStiffness: 1e8,
+        contactEquationRelaxation: 3,
+        frictionEquationStiffness: 1e8,
+        frictionEquationRegularizationTime: 3,
+    });
+    // Add contact material to the world
+    world.addContactMaterial(field_ball_cm);
+
+
+    let fieldShape = new CANNON.Cylinder(250,250,30,64);
+    field = new CANNON.Body({ type: CANNON.Body.STATIC, position: new CANNON.Vec3(0,0,0), material: fieldMaterial });
+    field.quaternion.set(0.7071,0,0,0.7071);
+    field.addShape(fieldShape);
+    world.addBody(field);
+    
+    let ballShape = new CANNON.Cylinder(25,25,10,16);
+    ball = new CANNON.Body({ mass: 10, position: new CANNON.Vec3(0,0,15), material: ballMaterial });
+    ball.quaternion.set(0.7071,0,0,0.7071);
+    ball.addShape(ballShape);
+    // ball.angularVelocity.set(0,0,0);
+    // ball.angularDamping = 0.25;
+    world.addBody(ball);
+    
+    // var q = new CANNON.Quaternion();
+    // q.setFromAxisAngle(new CANNON.Vec3(1,0,0),Math.PI / 2);
+    // cylinderShape2.transformAllPoints(new CANNON.Vec3(),q);
+
+
+}
+
+function initThree() {
+
+    scene = new Scene();
 
     let fieldCol = new MeshPhongMaterial({ color: "#00ff00", shininess: 0 });
     let fieldGeo = new CylinderBufferGeometry(250,250,30, 64);
-    let field = new Mesh( fieldGeo, fieldCol );
-    field.position.set(0,0,0);
-    field.rotation.set(Math.PI/2,0,0);
-    scene.add(field);
+    fieldMesh = new Mesh( fieldGeo, fieldCol );
+    // field.position.set(0,0,0);
+    // field.rotation.set(Math.PI/2,0,0);
+    scene.add(fieldMesh);
 
     let playerCol = new MeshPhongMaterial({ color: "#ff0000", shininess: 0 });
     let playerGeo = new TorusBufferGeometry(250, 10, 6, 6, Math.PI/6);
-    let player = new Mesh( playerGeo, playerCol );
+    player = new Mesh( playerGeo, playerCol );
     player.position.set(0,0,0);
     player.rotation.set(0,0,0);
     scene.add(player);
 
-    let camera = new OrthographicCamera( -480, 480, 320, -320, -400, 400 );
+    let ballCol = new MeshPhongMaterial({ color: "#0000ff", shininess: 0 });
+    let ballGeo = new CylinderBufferGeometry(25,25,10,16);
+    ballMesh = new Mesh( ballGeo, ballCol );
+    // ballMesh.position.set(0,100,0);
+    // ballMesh.rotation.set(0,Math.PI/2,0);
+    scene.add(ballMesh);
+    
+    camera = new PerspectiveCamera( 70, 800/600, 1, 1000 );
+    //camera = new OrthographicCamera( -480, 480, 320, -320, -400, 400 );
     camera.position.x = 0;
     camera.position.y = 0;
-    camera.position.z = 50;
+    camera.position.z = 500;
 
     let amblight = new AmbientLight( 0x808080 );
     scene.add( amblight );
@@ -45,20 +104,36 @@ function init() {
     let container = document.createElement( 'div' );
     document.body.appendChild( container );
 
-    let renderer = new WebGLRenderer({ alpha: true, antialias: true });
+    renderer = new WebGLRenderer({ alpha: true, antialias: true });
     renderer.setClearColor( 0xffffff, 0);
     renderer.setSize(800,600 );
     container.appendChild( renderer.domElement );
 
-    return { renderer: renderer, scene: scene, camera: camera, player: player };
-
 }
 
 function animate() {
-    g.player.rotation.z += 0.025;
-    g.renderer.render( g.scene, g.camera );
+    player.rotation.z += 0.025;
+    renderer.render( scene, camera );
     requestAnimationFrame( animate );
 }
 
-let g = init();
-animate(g);
+initCannon();
+initThree();
+animate();
+
+
+function animate() {
+    updatePhysics();
+    render();
+    requestAnimationFrame( animate );
+}
+function updatePhysics() {
+    world.step(timeStep);
+    fieldMesh.position.copy(field.position);
+    fieldMesh.quaternion.copy(field.quaternion);
+    ballMesh.position.copy(ball.position);
+    ballMesh.quaternion.copy(ball.quaternion);
+}
+function render() {
+    renderer.render( scene, camera );
+}
