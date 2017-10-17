971320daf03347f87aa05476d0381a7e7f99525d
Chris <chris@usablehq.com>
2017-10-17 19:48:39 +0100

    Getting somewhere - walls

--- src/pong.js
+++ src/pong.js
@@ -10,6 +10,7 @@ import { AmbientLight } from '../node_modules/three/src/lights/AmbientLight';
 import { PointLight } from '../node_modules/three/src/lights/PointLight';
 
 import { Mesh } from '../node_modules/three/src/objects/Mesh';
+import { BoxBufferGeometry } from '../node_modules/three/src/geometries/BoxGeometry';
 import { CylinderBufferGeometry } from '../node_modules/three/src/geometries/CylinderGeometry';
 import { TorusBufferGeometry } from '../node_modules/three/src/geometries/TorusGeometry';
 import { MeshPhongMaterial } from '../node_modules/three/src/materials/MeshPhongMaterial';
@@ -18,20 +19,33 @@ import { Object3D } from '../node_modules/three/src/core/Object3D';
 let world, scene, renderer, camera, player, field, fieldMesh, ball, ballMesh;
 var timeStep = 1/60;
 
-function initCannon() {
+function init() {
 
+    scene = new Scene();
+    
     world = new CANNON.World();
     world.gravity.set(0,0,-9.8);
     world.broadphase = new CANNON.NaiveBroadphase();
     world.solver.iterations = 10;
 
     var fieldMaterial = new CANNON.Material("fieldMaterial");
+    var wallMaterial = new CANNON.Material("wallMaterial");
     var ballMaterial = new CANNON.Material("ballMaterial");
     var field_ball_cm = new CANNON.ContactMaterial(fieldMaterial, ballMaterial, {
         friction: 0.001,
-        restitution: 0.03
+        restitution: 0.03,
+        contactEquationRelaxation: 10.0,
+        frictionEquationStiffness: 1
     });
     world.addContactMaterial(field_ball_cm);
+
+    var wall_ball_cm = new CANNON.ContactMaterial(wallMaterial, ballMaterial, {
+        friction: 0.1,
+        restitution: 1,
+        contactEquationRelaxation: 10.0,
+        frictionEquationStiffness: 1
+    });
+    world.addContactMaterial(wall_ball_cm);
 
     var quat = new CANNON.Quaternion(0.5, 0, 0, 0.5);
     quat.normalize();
@@ -42,23 +56,48 @@ function initCannon() {
     field.addShape(fieldShape, new CANNON.Vec3, quat);
     world.addBody(field);
 
-    let ballShape = new CANNON.Cylinder(25,25,10,16);
-    ball = new CANNON.Body({ mass: 1, position: new CANNON.Vec3(0,0,30), material: ballMaterial });
-    ball.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
-    ball.addShape(ballShape, new CANNON.Vec3, quat);
-    world.addBody(ball);
-
-}
-
-function initThree() {
-
-    scene = new Scene();
-
     let fieldCol = new MeshPhongMaterial({ color: "#00ff00", shininess: 0 });
     let fieldGeo = new CylinderBufferGeometry(250,250,30,64);
     fieldMesh = new Mesh( fieldGeo, fieldCol );
     fieldMesh.rotation.set(0,0,0);
     scene.add(fieldMesh);
+
+    let ballShape = new CANNON.Cylinder(25,25,10,16);
+    ball = new CANNON.Body({ mass: 5, position: new CANNON.Vec3(0,0,30), material: ballMaterial });
+    ball.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+    ball.addShape(ballShape, new CANNON.Vec3, quat);
+    world.addBody(ball);
+
+    let ballCol = new MeshPhongMaterial({ color: "#0000ff", shininess: 0 });
+    let ballGeo = new CylinderBufferGeometry(25,25,10,16);
+    ballMesh = new Mesh( ballGeo, ballCol );
+    scene.add(ballMesh);
+
+
+
+    let wallShape = new CANNON.Box(new CANNON.Vec3(100,5,15));
+    let wall = new CANNON.Body({ mass: 0, position: new CANNON.Vec3(0,200,30), material: wallMaterial });
+    wall.quaternion.setFromAxisAngle(new CANNON.Vec3(0,0,1),-Math.PI/8);
+    wall.addShape(wallShape);
+    world.addBody(wall);
+
+    let wallCol = new MeshPhongMaterial({ color: "#ff0000", shininess: 0 });
+    let wallGeo = new BoxBufferGeometry(200,10,30);
+    let wallMesh = new Mesh( wallGeo, wallCol );
+    wallMesh.position.copy(wall.position);
+    wallMesh.quaternion.copy(wall.quaternion);
+    scene.add(wallMesh);
+
+
+    let wallShape2 = new CANNON.Box(new CANNON.Vec3(100,5,15));
+    let wall2 = new CANNON.Body({ mass: 0, position: new CANNON.Vec3(0,-200,30), material: wallMaterial });
+    wall2.addShape(wallShape2);
+    world.addBody(wall2);
+
+    let wallMesh2 = new Mesh( wallGeo, wallCol );
+    wallMesh2.position.set(0,-200,30);
+    scene.add(wallMesh2);
+
 
     let playerCol = new MeshPhongMaterial({ color: "#ff0000", shininess: 0 });
     let playerGeo = new TorusBufferGeometry(250, 10, 6, 6, Math.PI/6);
@@ -67,15 +106,10 @@ function initThree() {
     player.rotation.set(0,0,0);
     scene.add(player);
 
-    let ballCol = new MeshPhongMaterial({ color: "#0000ff", shininess: 0 });
-    let ballGeo = new CylinderBufferGeometry(25,25,10,16);
-    ballMesh = new Mesh( ballGeo, ballCol );
-    scene.add(ballMesh);
-
     camera = new PerspectiveCamera( 70, 800/600, 1, 5000 );
     //camera = new OrthographicCamera( -480, 480, 320, -320, -400, 400 );
     camera.position.x = 0;
-    camera.position.y = -450;
+    camera.position.y = -150;
     camera.position.z = 500;
 
     camera.lookAt(fieldMesh.position);
@@ -117,11 +151,10 @@ function render() {
 }
 
 setTimeout(function(){
-    var worldPoint = new CANNON.Vec3(0,0,0);
-    var force = new CANNON.Vec3(0,2000,0);
-    ball.applyForce(force,worldPoint);
+    var worldPoint = new CANNON.Vec3(ball.position.x,ball.position.y,ball.position.z);
+    var impulse = new CANNON.Vec3(0,700,0);
+    ball.applyImpulse(impulse,worldPoint);
 }, 2000);
 
-initCannon();
-initThree();
+init();
 animate();
