def1ce19cf73b80a1b893405d88bd684cc66a230
Chris Neale <chris@komododigital.co.uk>
2018-05-14 16:39:16 +0100

    Updated to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 var path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 //const SriPlugin = require ('webpack-subresource-integrity');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: {
         app: ["./src/pong.js"]
     },
@@ -25,18 +26,12 @@ module.exports = {
             {
                 test: /\.(glsl|frag|vert)$/,
                 use: 'raw-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
             }
         ]
     },
     plugins: [
         new WebpackCleanupPlugin(),
+        new HtmlWebpackPlugin(),
         // new UglifyJSPlugin(),
         // new SriPlugin({
         //     hashFuncNames: ['sha256', 'sha384'],
@@ -45,7 +40,6 @@ module.exports = {
         new CopyWebpackPlugin([
           { from: 'assets' }
         ]),
-        new HtmlWebpackPlugin(),
-        new ExtractTextPlugin("pixelheart.css"),
+        // new ExtractTextPlugin("pixelheart.css"),
     ]
 };
